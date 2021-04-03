e2b71f245f165e76ad62ace56824c5ac5e34c559
USER <user@AL01742072.local>
2021-04-04 00:50:28 +0900

    apply prettier at javascript

--- 01_ColroEditor/script.js
+++ 01_ColroEditor/script.js
@@ -1,23 +1,64 @@
-const NUM_OF_BUTTONS = 7
-const COLORS_ENG = ["red", "orange", "yellow", "green", "blue", "navy", "purple"]
-const COLORS_KOR = ["빨간색", "주황색", "노란색", "초록색", "파란색", "남색", "보라색"]
+const NUM_OF_BUTTONS = 7;
+const COLORS_ENG = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "navy",
+  "purple",
+];
+const COLORS_KOR = [
+  "빨간색",
+  "주황색",
+  "노란색",
+  "초록색",
+  "파란색",
+  "남색",
+  "보라색",
+];
 
-var colorButtons = document.getElementsByClassName('colors')
-var result = document.getElementById('result')
+var colorButtons = document.getElementsByClassName("colors");
+var result = document.getElementById("result");
 
-for(var i=0; i<NUM_OF_BUTTONS; i++) {
-    colorButtons[i].innerHTML = COLORS_KOR[i]
-    colorButtons[i].style.backgroundColor = COLORS_ENG[i]
+for (var i = 0; i < NUM_OF_BUTTONS; i++) {
+  colorButtons[i].innerHTML = COLORS_KOR[i];
+  colorButtons[i].style.backgroundColor = COLORS_ENG[i];
 }
 
 function click(c) {
-    result.style.backgroundColor = c;
+  result.style.backgroundColor = c;
 }
 
-colorButtons[0].onclick = () => {click(COLORS_ENG[0])}
-colorButtons[1].onclick = () => {click(COLORS_ENG[1])}
-colorButtons[2].onclick = () => {click(COLORS_ENG[2])}
-colorButtons[3].onclick = () => {click(COLORS_ENG[3])}
-colorButtons[4].onclick = () => {click(COLORS_ENG[4])}
-colorButtons[5].onclick = () => {click(COLORS_ENG[5])}
-colorButtons[6].onclick = () => {click(COLORS_ENG[6])}
+function anotherColor() {
+  var rgbCode = prompt("RGB 코드를 입력하세요. ex)#000000");
+  console.log(rgbCode);
+}
+
+colorButtons[0].onclick = () => {
+  click(COLORS_ENG[0]);
+};
+
+colorButtons[1].onclick = () => {
+  click(COLORS_ENG[1]);
+};
+
+colorButtons[2].onclick = () => {
+  click(COLORS_ENG[2]);
+};
+
+colorButtons[3].onclick = () => {
+  click(COLORS_ENG[3]);
+};
+
+colorButtons[4].onclick = () => {
+  click(COLORS_ENG[4]);
+};
+
+colorButtons[5].onclick = () => {
+  click(COLORS_ENG[5]);
+};
+
+colorButtons[6].onclick = () => {
+  click(COLORS_ENG[6]);
+};
